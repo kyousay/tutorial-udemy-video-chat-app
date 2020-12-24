8ead9c136528312c1b5229ec30d5ab913b3a85db
yusei <yusei5884@gmail.com>
2020-12-24 23:14:02 +0900

    第８回他ユーザーのカメラ情報を表示

--- public/script.js
+++ public/script.js
@@ -12,18 +12,32 @@ const addVideoStream = (video, stream) => {
     videoWrap.append(video);
 }
 
+const connectToNewUser = (userId, stream) => {
+    const call = myPeer.call(userId, stream);
+    const video = document.createElement("video");
+    call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+    });
+    call.on("close", () => {
+        video.remove();
+    });
+}
+
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true,
 }).then(stream => {
     addVideoStream(myVideo, stream);
+
+    myPeer.on("call", call => {
+        call.answer(stream);
+    });
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream)
+    });
 });
 
 myPeer.on('open', userId => {
     socket.emit('join-room', ROOM_ID, userId);
 });
 
-
-socket.on('user-connected', userId => {
-    console.log('userId=', userId);
-})
